29eb54004fea2cbd2df7460e70ab5db3a830bfec
Carlos Castro <61478783+carloskastro@users.noreply.github.com>
2023-06-01 15:11:52 -0500

    Add files via upload

---
+++ app/conn.php
@@ -0,0 +1,14 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="sena";
+
+try{
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8",$username,$password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	echo "Conexión OK";
+}catch(PDOException $e){
+	echo "Conexión Falló".$e->getMessage();
+}
+?>
--- app/index.php
+++ app/index.php
@@ -12,33 +12,56 @@
 	<meta name="keywords" content="SENA, sena, Sena, Aplicativo, aplicativo, web, Web">
 
 	<!-- Favicon-->
-	<link href="../media/img/logo1.png" rel="icon" type="image/x-icon">
+	<link href="../media/img/logo.png" rel="icon" type="image/x-icon">
 
 	<!--Styles Bootstrap 5.3.0 alpha3-->
 	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+	<!--Library Icons Bootstrap-->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 	<script type="text/javascript" src="../assets/js/bootstrap.bundle.js"></script>
 </head>
-<body>
+<body class="bg-dark">
+	<?php
+	require_once 'conn.php';
+	session_start();
+
+
+	?>
 	<main>
 		<div class="container mt-3">
-			<h2 class="text-center">Formulario de inicio</h2>
-			<form action="/action_page.php">
-				<div class="mb-3 mt-3">
-					<label for="email">Usuario:</label>
-					<input type="email" class="form-control" id="email" placeholder="Digite su usuario" name="email">
+			<div class="card">
+				<div class="card-header">
+					<div class="row">
+					<div class="col text-left">
+						<img class="mb-2" src="../media/img/logo.png" alt="Logo Sena" style="height: 48px">
+					</div>
+					<div class="col text-end">
+						<a href="../"><i class="text-danger bi bi-x-circle-fill"></i></a>
+					</div>
 				</div>
-				<div class="mb-3">
-					<label for="pwd">Contraseña:</label>
-					<input type="password" class="form-control" id="pwd" placeholder="Digite su contraseña" name="pswd">
+					
 				</div>
-				<div class="form-check mb-3">
-					<label class="form-check-label">
-						<input class="form-check-input" type="checkbox" name="remember"> Remember me
-					</label>
+				<div class="card-body">
+					<p class="display-6 text-center">Inicio de Sesión</p>
+					<form action="" method="post" enctype="application/x-www-form-urlencoded">
+						<div class="mb-3 mt-3">
+							<label for="user">Usuario:</label>
+							<input type="text" class="form-control"  placeholder="Digite su usuario" name="user">
+						</div>
+						<div class="mb-3">
+							<label for="pwd">Contraseña:</label>
+							<input type="password" class="form-control" placeholder="Digite su contraseña" name="pass">
+						</div>
+						<div class="form-check mb-3">
+							<label class="form-check-label">
+								<input class="form-check-input" type="checkbox" name="remember"> Recuerdáme
+							</label>
+						</div>
+						<button type="submit" class="btn btn-primary" name="validar">Entrar</button>
+						<a href="reg_usuario" class="btn btn-link">Registrar</a>
+					</form>
 				</div>
-				<button type="submit" class="btn btn-primary">Submit</button>
-				<a href="reg_usuario.php" class="btn btn-primary">Registrar</a>
-			</form>
+			</div>
 		</div>
 	</main>
 	
